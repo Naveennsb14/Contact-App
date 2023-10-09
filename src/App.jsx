b912b457c047df0e248a3792cd8fcd48b95c2bc8
Naveen <baghelnaveensingh@gmail.com>
2023-10-10 00:31:16 +0530

    added search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,98 @@
-import Navbar from "./components/Navbar"
-import { FiSearch } from "react-icons/fi"
-import { AiFillPlusCircle } from "react-icons/ai"
-import { useEffect, useState } from "react"
-import {collection, getDocs} from "firebase/firestore"
-import { db } from "./config/firebase"
-import { HiOutlineUserCircle } from 'react-icons/hi'
-import { IoMdTrash } from "react-icons/io"
-import { RiEditCircleLine } from "react-icons/ri"
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { db } from "./config/firebase";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ContactCard from "./components/ContactCard";
+import Modal from "./components/Modal";
+import AddandUpdateContact from "./components/AddandUpdateContact";
+import useDisclouse from "./hooks/useDisclouse";
 
 
 const App = () => {
+  const [contacts, setContacts] = useState([]);
 
-  const [contacts, setContacts]= useState([]);
+  const{isOpen, onClose, onOpen} = useDisclouse()
 
-  useEffect(()=>{
-    const getContacts = async () =>{
-
+  useEffect(() => {
+    const getContacts = async () => {
       try {
         const contactsRef = collection(db, "contacts");
-        const contactsSnapshot = await getDocs(contactsRef);
-        const contactList = contactsSnapshot.docs.map((doc)=>{
-          return {
-            id: doc.id,
-            ...doc.data()
-          }
+
+        onSnapshot(contactsRef,(snapshot)=>{
+          const contactList = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          setContacts(contactList);
+          return contactList;
         })
-        setContacts(contactList);
-      } catch (error) {
         
-      }
+      } catch (error) {}
     };
 
     getContacts();
+  }, []);
 
-  },[])
+  const filterContacts = (e) => {
+    const value = e.target.value;
 
+    const contactsRef = collection(db, "contacts");
 
+    onSnapshot(contactsRef, (snapshot) => {
+      const contactLists = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      const filteredContacts = contactLists.filter((contact) =>
+        contact.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      setContacts(filteredContacts);
+
+      return filteredContacts;
+    });
+  };
+
+  
 
   return (
-    <div className="max-w-[370px] mx-auto px-4">
-      <Navbar/>
-      <div className="flex gap-2">
-      <div className="flex relative items-center ml-1 flex-grow" >
-      <FiSearch className="text-white text-3xl absolute"/>
-        <input type="text" className=" text-white pl-9 flex-grow bg-transparent border border-white
-        rounded-md h-10" />
+    <>
+      <div className="max-w-[370px] mx-auto px-4">
+        <Navbar />
+        <div className="flex gap-1">
+          <div className="flex relative items-center ml-1 flex-grow">
+            <FiSearch className="text-white text-3xl absolute" />
+            <input onChange={filterContacts}
+              type="text"
+              className=" text-white pl-9 flex-grow bg-transparent border border-white
+        rounded-md h-10"
+            />
+          </div>
+          <AiFillPlusCircle
+            onClick={onOpen}
+            className="text-5xl text-white cursor-pointer"
+          />
+        </div>
+        <div className="mt-4 flex flex-col gap-4">
+          {contacts.map((contact) => (
+            <ContactCard key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
-        <AiFillPlusCircle className="text-5xl text-white cursor-pointer"/>
+      <AddandUpdateContact onClose={onClose} isOpen={isOpen}/>
+      <ToastContainer position="bottom-center"/>
       
-      </div>
-      <div>
-        {contacts.map((contact)=>(
-            <div key={contact.id}>
-              <HiOutlineUserCircle/>
-              <div className="text-white">
-                <h2 className="">{contact.name}</h2>
-                <p className="text-white">{contact.email}</p>
-              </div>
-              <div>
-              <RiEditCircleLine/>
-                <IoMdTrash/>
-              </div>
-            </div>
+    </>
+  );
+};
 
-        ))
-        }
-      </div>
-    </div>
- 
-  )
-}
-
-export default App
+export default App;
